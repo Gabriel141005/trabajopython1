53af98c9261df05936c469d84fd0874843b3f12d
Gabriel <gabrielang141005@gmail.com>
2024-09-25 11:47:52 -0600

    primer commit en pycharm

---
+++ variables.py
@@ -0,0 +1,16 @@
+from platform import uname
+
+nombre = "Gabriel"
+edad = 18
+altura = 1.71
+nacionalidad_mx= True
+
+# esto es un comentario en una linea
+
+"""
+esto es uname
+comentario de
+multiples lineas
+"""
+
+print nombre)
